ff4d4c8c1a03b2f8c04a59f8667c7f302e9cb756
Aqib Munir <aqibmunir724@gmail.com>
2026-01-21 23:25:25 +0500

    LL

--- Arrays/Creating our own list/0-creating_our_own_list.py
+++ Arrays/Creating our own list/0-creating_our_own_list.py
@@ -1,2 +1,33 @@
 import ctypes
 
+class Meralist:
+    def __init__(self):
+        self.size = 1
+        self.n =0
+        # Create a C type array with size = self.size
+        self.A = self.__make_array(self.size)
+    
+    def __make_array(self, capacity):
+        return (capacity * ctypes.py_object)()
+    
+    def __len__(self):
+        return self.n
+    
+    def append(self, item):
+        if self.n == self.size:
+            #resize
+            pass
+        # Append
+        self.A[self.n] = item
+        self.n += 1
+
+    def __str__(self):
+        result = ''
+        for i in range(self.n):
+            result += str(self.A[i]) + ", "
+        return f"[{result}]"
+    
+L = Meralist()
+L.append("Hello World")
+L.append("Hello World")
+print(L)
